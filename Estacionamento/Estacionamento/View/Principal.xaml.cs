756287c607d431359fff28aa81eba97a61e63e01
henmerlin <henmerlin@gmail.com>
2016-06-23 21:09:48 -0300

    commit

--- Estacionamento/Estacionamento/View/Principal.xaml.cs
+++ Estacionamento/Estacionamento/View/Principal.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Estacionamento
+namespace Estacionamento.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Principal : Window
     {
-        public MainWindow()
+        public Principal()
         {
             InitializeComponent();
         }

2cd2a71f08329f6f1ab4b0318056b54240ac46b9
henmerlin <henmerlin@gmail.com>
2016-06-30 19:18:48 -0300

    teste

---
+++ Estacionamento/Estacionamento/DAL/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento.DAL
+{
+    class Context : DbContext
+    {
+        public Context()
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+
+    }
+}

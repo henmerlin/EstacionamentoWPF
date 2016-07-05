bf8bfe3bb9eff7161e546fd3b779b19e02d188f8
henmerlin <henmerlin@gmail.com>
2016-07-05 02:07:44 -0300

    Commit

--- Estacionamento/Estacionamento/Model/Modelo.cs
+++ Estacionamento/Estacionamento/Model/Modelo.cs
@@ -1,12 +1,14 @@
 ﻿using Estacionamento.DAL;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Estacionamento.Model
 {
+    [Table("Modelos")]
     class Modelo
     {
         public int Id { get; set; }
--- Estacionamento/Estacionamento/Model/Veiculo.cs
+++ Estacionamento/Estacionamento/Model/Veiculo.cs
@@ -1,10 +1,12 @@
 ﻿using Estacionamento.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
 namespace Estacionamento.DAL
 {
+    [Table("Veiculos")]
     class Veiculo
     {
         public int Id { get; set; }
